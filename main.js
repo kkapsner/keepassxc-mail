dc49fac6d27ec7c101aa520d977d2fe7bbb6d4b5
kkapsner <github@mail.kkapsner.de>
2024-03-27 15:53:27 +0100

    Added timeout to password saving
    Fixes #91 and #90

--- main.js
+++ main.js
@@ -456,56 +456,99 @@ browser.runtime.onMessage.addListener(function(message, tab){
 	}
 });
 
+const timeoutSymbol = Symbol("timeout");
+async function storeCredentials(credentialInfo, uuid){
+	
+	log("Get or create password group");
+	const group = await Promise.any([
+		keepass.createNewGroup(null, ["KeePassXC-Mail Passwords"]),
+		wait(1 * 60 * 1000, timeoutSymbol)
+	]);
+	if (group === timeoutSymbol){
+		log("Timeout while creating password group: using default password manager");
+		messageModal(
+			browser.i18n.getMessage("createPasswordGroup.timeout.title"),
+			browser.i18n.getMessage("createPasswordGroup.timeout.message")
+				.replace("{account}", credentialInfo.login)
+		);
+		return false;
+	}
+	
+	log("Saving password to database for", credentialInfo.login, "at", credentialInfo.host);
+	log("Using uuid:", uuid);
+	
+	const result = await Promise.any([
+		keepass.updateCredentials(null, [
+			uuid,
+			credentialInfo.login, credentialInfo.password, credentialInfo.host,
+			group.name, group.uuid
+		]),
+		wait(2 * 60 * 1000, timeoutSymbol)
+	]);
+	if (result === timeoutSymbol){
+		log("Timeout while saving: using default password manager");
+		messageModal(
+			browser.i18n.getMessage("savePassword.timeout.title"),
+			browser.i18n.getMessage("savePassword.timeout.message")
+				.replace("{account}", credentialInfo.login)
+		);
+		return false;
+	}
+	else {
+		log("Saving done");
+		return true;
+	}
+}
 browser.credentials.onNewCredential.addListener(async function(credentialInfo){
 	log("Got new password for", credentialInfo.login, "at", credentialInfo.host);
 	const {saveNewCredentials, autoSaveNewCredentials} = (await browser.storage.local.get({
 		saveNewCredentials: true,
 		autoSaveNewCredentials: false
 	}));
-	if (saveNewCredentials){
-		await isKeepassReady();
-		const existingCredentials = (await keepass.retrieveCredentials(
-			false,
-			[credentialInfo.host, credentialInfo.host]
-		)).filter(function (credential){
-			return (
-				true === credentialInfo.login ||
-				credential.login.toLowerCase?.() === credentialInfo.login.toLowerCase?.()
-			);
-		});
-		if (!existingCredentials.some(function(credential){
-			return credential.password === credentialInfo.password;
-		})){
-			const cachedId = credentialInfo.login?
-				`${credentialInfo.login}@${credentialInfo.host}`:
-				credentialInfo.host;
-			const cached = selectedEntries.get(cachedId) || null;
-			const {save, uuid} = autoSaveNewCredentials?
-				{save: true, uuid: cached?.uuid || null}:
-				await savingPasswordModal(
-					credentialInfo.host,
-					credentialInfo.login,
-					existingCredentials.map(function (data){
-						return {
-							name: data.name,
-							login: data.login,
-							uuid: data.uuid,
-							preselected: data.uuid === cached?.uuid
-						};
-					})
-				);
-			if (save){
-				log("Get or create password group");
-				const group = await keepass.createNewGroup(null, ["KeePassXC-Mail Passwords"]);
-				log("Saving password to database for", credentialInfo.login, "at", credentialInfo.host);
-				log("Using uuid:", uuid);
-				await keepass.updateCredentials(null,
-					[uuid, credentialInfo.login, credentialInfo.password, credentialInfo.host, group.name, group.uuid]
-				);
-				log("Saving done");
-				return true;
-			}
-		}
+	if (!saveNewCredentials){
+		log("password saving is disabled in the settings");
+		return false;
 	}
-	return false;
+	
+	await isKeepassReady();
+	const existingCredentials = (await keepass.retrieveCredentials(
+		false,
+		[credentialInfo.host, credentialInfo.host]
+	)).filter(function (credential){
+		return (
+			true === credentialInfo.login ||
+			credential.login.toLowerCase?.() === credentialInfo.login.toLowerCase?.()
+		);
+	});
+	if (existingCredentials.some(function(credential){
+		return credential.password === credentialInfo.password;
+	})){
+		log("the password is already stored");
+		return true;
+	}
+	
+	const cachedId = credentialInfo.login?
+		`${credentialInfo.login}@${credentialInfo.host}`:
+		credentialInfo.host;
+	const cached = selectedEntries.get(cachedId) || null;
+	const {save, uuid} = autoSaveNewCredentials?
+		{save: true, uuid: cached?.uuid || null}:
+		await savingPasswordModal(
+			credentialInfo.host,
+			credentialInfo.login,
+			existingCredentials.map(function (data){
+				return {
+					name: data.name,
+					login: data.login,
+					uuid: data.uuid,
+					preselected: data.uuid === cached?.uuid
+				};
+			})
+		);
+	if (!save){
+		log("the user decided to not store the password");
+		return true;
+	}
+	
+	return await storeCredentials(credentialInfo, uuid);
 });
