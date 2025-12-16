878ace0188940b161b07bc296cfe40749710f230
kkapsner <github@mail.kkapsner.de>
2025-12-16 16:32:35 +0100

    Add new prompt functions

--- experiment/modules/prompter/loginManagerAuthPrompter.sys.js
+++ experiment/modules/prompter/loginManagerAuthPrompter.sys.js
@@ -1,7 +1,41 @@
+/* globals Ci */
 import { initPromptFunctions, registerPromptFunctions } from "./utils.sys.js";
 import { LoginManagerAuthPrompter } from "resource://gre/modules/LoginManagerAuthPrompter.sys.mjs";
 
 const promptFunctions = [
+	{
+		name: "asyncPromptPassword",
+		isAsync: true,
+		promptType: "promptPassword",
+		titleIndex: 0,
+		textIndex: 1,
+		realmIndex: 2,
+		savePasswordIndex: 3,
+		savePasswordValue: Ci.nsIAuthPrompt.SAVE_PASSWORD_NEVER,
+		passwordObjectIndex: 4,
+	},
+	{
+		name: "asyncPromptUsernameAndPassword",
+		isAsync: true,
+		promptType: "promptUserAndPass",
+		titleIndex: 0,
+		textIndex: 1,
+		realmIndex: 2,
+		savePasswordIndex: 3,
+		savePasswordValue: Ci.nsIAuthPrompt.SAVE_PASSWORD_NEVER,
+		usernameObjectIndex: 4,
+		passwordObjectIndex: 5,
+	},
+	{
+		name: "prompt",
+		promptType: "promptPassword",
+		titleIndex: 0,
+		textIndex: 1,
+		realmIndex: 2,
+		savePasswordIndex: 3,
+		savePasswordValue: Ci.nsIAuthPrompt.SAVE_PASSWORD_NEVER,
+		passwordObjectIndex: 5,
+	},
 	{
 		name: "promptPassword",
 		promptType: "promptPassword",
@@ -17,7 +51,7 @@ const promptFunctions = [
 		textIndex: 1,
 		realmIndex: 2,
 		passwordObjectIndex: 5,
-	}
+	},
 ];
 initPromptFunctions(promptFunctions, LoginManagerAuthPrompter.prototype);
 registerPromptFunctions(promptFunctions);
